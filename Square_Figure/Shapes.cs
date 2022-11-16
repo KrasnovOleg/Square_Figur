d471a532a7747427383d3a88dc80bb256f22f56d
KrasnovOleg <krasnovoleg232@gmail.com>
2022-11-17 06:52:59 +0700

    Добавьте файлы проекта.

---
+++ Square_Figure/Shapes.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Figure
+{
+    public class Shapes
+    {
+        public  double Circle(double x)//Площадь круга
+        {
+            double Square = x * 3.14;
+
+            Console.Write("Периметр круга = ");
+            Console.WriteLine(Square);
+            return Square;
+        }
+        public  double box(double x, double y)//Площадь Квадрата и прямоугольника
+        {
+            if (x == y)
+            {
+                double Square = x * y;
+
+                Console.Write("Периметр квадрата = ");
+                Console.WriteLine(Square);
+                return Square;
+            }
+            else
+            {
+                double Square = x * y;
+
+                Console.Write("Периметр Прямоугольника = ");
+                Console.WriteLine(Square);
+                return Square;
+            }
+
+        }
+
+        public  double Triangle(double x, double y, double z)//Площадь Треугольника
+        {
+            double s=0, p;
+
+
+            if (Math.Pow(x, 2) + Math.Pow(y, 2) == Math.Pow(z, 2)
+            || Math.Pow(y, 2) + Math.Pow(z, 2) == Math.Pow(x, 2)
+            || Math.Pow(z, 2) + Math.Pow(x, 2) == Math.Pow(y, 2))
+            {
+                Console.WriteLine("Это Прямоугольный треугольник!");
+                p = (x + y + z) / 2;
+                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                Console.Write("Периметр треугольника = " );
+                Console.WriteLine(s);
+               
+            }
+            else if (x + y <= z || x + z <= y || y + z <= x)
+            {
+                Console.WriteLine("Это не треугольник!");
+                return 0;
+            }
+            else if (x == y && y == z)
+            {
+                Console.WriteLine("Это равносторонний треугольник!");
+                p = (x + y + z) / 2;
+               double s1 = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                 s = Math.Round(s1, 3);
+
+                Console.Write("Периметр треугольника = ");
+                Console.WriteLine(s);
+               
+            }
+            else if (x == y || x == z || y == z)
+            {
+                Console.WriteLine("Это равнобедренный треугольник!");
+                p = (x + y + z) / 2;
+                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                Console.Write("Периметр треугольника = ");
+                Console.WriteLine(s);
+                
+            }
+            else
+            {
+                Console.WriteLine("Это треугольник.");
+                p = (x + y + z) / 2;
+                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                Console.Write("Периметр треугольника = ");
+                Console.WriteLine(s);
+               
+            }
+            return s;
+        }
+
+
+        public  double Figure(double x=0, double y=0, double z=0)//Площадь нужной фигуры
+        {
+            // x = 0, y = 0, z = 0;
+            double s = 0;
+            if (x != 0 && y == 0 && z == 0)
+            {
+                Circle(x);
+                s= Circle(x);
+            }
+            else if (x != 0 && y != 0 && z == 0)
+            {
+                box(x, y);
+                s = box(x, y);
+
+            }
+            else if (x != 0 && y != 0 && z != 0)
+            {
+                Triangle(x, y, z);
+                s = Triangle(x, y, z);
+
+            }
+            else
+            {
+                Console.WriteLine("Этой формулы нет ");
+                
+            }
+            return s;
+        }
+
+
+        //Не особо понял про лёгкость добавления других фигур, но надеюсь это про разделение формул, что я сделал
+    }
+}
---
+++ Square_FigureTests/ShapesTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Square_Figure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Figure.Tests
+{
+    [TestClass()]
+    public class ShapesTests
+    {
+        [TestMethod()]
+        public void CircleTest()
+        {
+            double x = 3;
+            double expected = 9.42;
+
+            Shapes c = new Shapes();
+            double actual = c.Circle(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void boxTest()
+        {
+            double x = 3;
+            double y = 3;
+            double expected = 9;
+
+            Shapes c = new Shapes();
+            double actual = c.box(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void boxTest2()
+        {
+            double x = 3;
+            double y = 4;
+            double expected = 12;
+
+            Shapes c = new Shapes();
+            double actual = c.box(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Triangle_rectangular_Test()
+        {
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            double expected = 6;
+
+            Shapes c = new Shapes();
+            double actual = c.Triangle(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Triangle_isosceles_Test()
+        {
+            double x = 3;
+            double y = 3;
+            double z = 3;
+            double expected = 3.897;
+
+            Shapes c = new Shapes();
+            double actual = c.Triangle(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Triangle_error_Test()
+        {
+            double x = 1;
+            double y = 1;
+            double z = 9;
+            double expected = 0;
+
+            Shapes c = new Shapes();
+            double actual = c.Triangle(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void Figure_Circle_Test()
+        {
+            double x = 3;
+            double expected = 9.42;
+
+            Shapes c = new Shapes();
+            double actual = c.Figure(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Figure_box_Test()
+        {
+            double x = 3;
+            double y = 3;
+            double expected = 9;
+
+            Shapes c = new Shapes();
+            double actual = c.Figure(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Figure_box2_Test()
+        {
+            double x = 3;
+            double y = 4;
+            double expected = 12;
+
+            Shapes c = new Shapes();
+            double actual = c.Figure(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Figure_Triangle_Test()
+        {
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            double expected = 6;
+
+            Shapes c = new Shapes();
+            double actual = c.Figure(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Figure_Triangle_isosceles_Test()
+        {
+            double x = 3;
+            double y = 3;
+            double z = 3;
+            double expected = 3.897;
+
+            Shapes c = new Shapes();
+            double actual = c.Figure(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
