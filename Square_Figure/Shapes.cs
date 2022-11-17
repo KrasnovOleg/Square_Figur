5339fc360ee34792dcbafaf668a3de6af0194d42
KrasnovOleg <krasnovoleg232@gmail.com>
2022-11-17 13:50:11 +0700

    Создано исключение на ошибки try catch

--- Square_Figure/Shapes.cs
+++ Square_Figure/Shapes.cs
@@ -8,116 +8,161 @@ namespace Square_Figure
 {
     public class Shapes
     {
-        public  double Circle(double x)//Площадь круга
+        public double Circle(double x)//Площадь круга
         {
-            double Square = x * 3.14;
-
-            Console.Write("Периметр круга = ");
-            Console.WriteLine(Square);
-            return Square;
-        }
-        public  double box(double x, double y)//Площадь Квадрата и прямоугольника
-        {
-            if (x == y)
+            try
             {
-                double Square = x * y;
+                x = Math.Abs(x);
+                double Square = x * 3.14;
 
-                Console.Write("Периметр квадрата = ");
+                Console.Write("Периметр круга = ");
                 Console.WriteLine(Square);
                 return Square;
             }
-            else
+            catch
             {
-                double Square = x * y;
-
-                Console.Write("Периметр Прямоугольника = ");
-                Console.WriteLine(Square);
-                return Square;
-            }
-
-        }
-
-        public  double Triangle(double x, double y, double z)//Площадь Треугольника
-        {
-            double s=0, p;
-
-
-            if (Math.Pow(x, 2) + Math.Pow(y, 2) == Math.Pow(z, 2)
-            || Math.Pow(y, 2) + Math.Pow(z, 2) == Math.Pow(x, 2)
-            || Math.Pow(z, 2) + Math.Pow(x, 2) == Math.Pow(y, 2))
-            {
-                Console.WriteLine("Это Прямоугольный треугольник!");
-                p = (x + y + z) / 2;
-                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
-                Console.Write("Периметр треугольника = " );
-                Console.WriteLine(s);
-               
-            }
-            else if (x + y <= z || x + z <= y || y + z <= x)
-            {
-                Console.WriteLine("Это не треугольник!");
                 return 0;
-            }
-            else if (x == y && y == z)
-            {
-                Console.WriteLine("Это равносторонний треугольник!");
-                p = (x + y + z) / 2;
-               double s1 = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
-                 s = Math.Round(s1, 3);
 
-                Console.Write("Периметр треугольника = ");
-                Console.WriteLine(s);
-               
             }
-            else if (x == y || x == z || y == z)
+
+        }
+        public double box(double x, double y)//Площадь Квадрата и прямоугольника
+        {
+            try
             {
-                Console.WriteLine("Это равнобедренный треугольник!");
-                p = (x + y + z) / 2;
-                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
-                Console.Write("Периметр треугольника = ");
-                Console.WriteLine(s);
-                
+
+
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+
+                if (x == y)
+                {
+                    double Square = x * y;
+
+                    Console.Write("Периметр квадрата = ");
+                    Console.WriteLine(Square);
+                    return Square;
+                }
+                else
+                {
+                    double Square = x * y;
+
+                    Console.Write("Периметр Прямоугольника = ");
+                    Console.WriteLine(Square);
+                    return Square;
+                }
             }
-            else
+            catch
             {
-                Console.WriteLine("Это треугольник.");
-                p = (x + y + z) / 2;
-                s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
-                Console.Write("Периметр треугольника = ");
-                Console.WriteLine(s);
-               
+                return 0;
+
             }
-            return s;
+        }
+
+        public double Triangle(double x, double y, double z)//Площадь Треугольника
+        {
+            try
+            {
+
+
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+                z = Math.Abs(z);
+
+                double s = 0, p;
+
+
+                if (Math.Pow(x, 2) + Math.Pow(y, 2) == Math.Pow(z, 2)
+                || Math.Pow(y, 2) + Math.Pow(z, 2) == Math.Pow(x, 2)
+                || Math.Pow(z, 2) + Math.Pow(x, 2) == Math.Pow(y, 2))
+                {
+                    Console.WriteLine("Это Прямоугольный треугольник!");
+                    p = (x + y + z) / 2;
+                    s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                    Console.Write("Периметр треугольника = ");
+                    Console.WriteLine(s);
+
+                }
+                else if (x + y <= z || x + z <= y || y + z <= x)
+                {
+                    Console.WriteLine("Это не треугольник!");
+                    return 0;
+                }
+                else if (x == y && y == z)
+                {
+                    Console.WriteLine("Это равносторонний треугольник!");
+                    p = (x + y + z) / 2;
+                    double s1 = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                    s = Math.Round(s1, 3);
+
+                    Console.Write("Периметр треугольника = ");
+                    Console.WriteLine(s);
+
+                }
+                else if (x == y || x == z || y == z)
+                {
+                    Console.WriteLine("Это равнобедренный треугольник!");
+                    p = (x + y + z) / 2;
+                    s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                    Console.Write("Периметр треугольника = ");
+                    Console.WriteLine(s);
+
+                }
+                else
+                {
+                    Console.WriteLine("Это треугольник.");
+                    p = (x + y + z) / 2;
+                    s = (float)Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                    Console.Write("Периметр треугольника = ");
+                    Console.WriteLine(s);
+
+                }
+                return s;
+            }
+            catch
+            {
+                return 0;
+
+            }
         }
 
 
-        public  double Figure(double x=0, double y=0, double z=0)//Площадь нужной фигуры
+        public double Figure(double x = 0, double y = 0, double z = 0)//Площадь нужной фигуры
         {
-            // x = 0, y = 0, z = 0;
-            double s = 0;
-            if (x != 0 && y == 0 && z == 0)
+            try
             {
-                Circle(x);
-                s= Circle(x);
-            }
-            else if (x != 0 && y != 0 && z == 0)
-            {
-                box(x, y);
-                s = box(x, y);
-
-            }
-            else if (x != 0 && y != 0 && z != 0)
-            {
-                Triangle(x, y, z);
-                s = Triangle(x, y, z);
-
-            }
-            else
-            {
-                Console.WriteLine("Этой формулы нет ");
                 
+                // x = 0, y = 0, z = 0;
+                double s = 0;
+                if (x != 0 && y == 0 && z == 0)
+                {
+                    Circle(x);
+                    s = Circle(x);
+                }
+                else if (x != 0 && y != 0 && z == 0)
+                {
+                    box(x, y);
+                    s = box(x, y);
+
+                }
+                else if (x != 0 && y != 0 && z != 0)
+                {
+                    Triangle(x, y, z);
+                    s = Triangle(x, y, z);
+
+                }
+                else
+                {
+                    Console.WriteLine("Этой формулы нет ");
+
+                }
+                return s;
             }
-            return s;
+            catch
+            {
+                return 0;
+
+            }
         }
 
 
